48a08c3eca242e759f4f2cb267c55771393126e9
Shahriar Ahmed Shovon <shovonshahriar01@gmail.com>
2022-07-04 13:40:07 +0600

    Shortlink Format changed

--- Controllers/link.controller.js
+++ Controllers/link.controller.js
@@ -30,7 +30,7 @@ const linkAdd = async (req, res) => {
         let shortLink = req.body.shortLink
         shortLink.replaceAll(" ", "")
         const existLink = await linkModel.findOne({
-            shortLink: `${process.env.CLIENT_DOMAIN}/${shortLink}`
+            shortLink: `${process.env.CLIENT_DOMAIN}/l/${shortLink}`
         })
         if (existLink) {
             res.status(200).json({
@@ -41,7 +41,7 @@ const linkAdd = async (req, res) => {
             const newLink = await linkModel({
                 linkId: linkId,
                 originalLink: req.body.originalLink,
-                shortLink: `${process.env.CLIENT_DOMAIN}/${shortLink || uniqueURL()}`,
+                shortLink: `${process.env.CLIENT_DOMAIN}/l/${shortLink || uniqueURL()}`,
                 user: req.userId,
                 status: "Active"
             })
@@ -66,7 +66,7 @@ const linkUpdate = async (req, res) => {
     try {
         let shortLink = req.body.shortLink
         shortLink.replaceAll(" ", "")
-        shortLink = `${process.env.CLIENT_DOMAIN}/${shortLink}`
+        shortLink = `${process.env.CLIENT_DOMAIN}/l/${shortLink}`
         const existLink = await linkModel.findOne({ linkId: req.params.linkId })
         if (existLink) {
             const duplicateLink = await linkModel.findOne({ shortLink: shortLink })
